cb27db213b54f6dd00d30a238dee447553b933cc
Niklas Lutze <niklas.lutze@student.kit.edu>
2023-11-15 18:10:42 +0000

    WIP

--- data_profiling.py
+++ data_profiling.py
@@ -1,58 +1,55 @@
-from pandas import read_csv, DataFrame
+from dslabs_functions import set_chart_labels
+from dslabs_functions import plot_multibar_chart
+from pandas import Series
+from pandas import read_csv, DataFrame, Series, to_numeric, to_datetime
 from matplotlib.pyplot import figure, savefig, show
 from dslabs_functions import plot_bar_chart
+from matplotlib.pyplot import savefig, show
+from dslabs_functions import get_variable_types
+from numpy import ndarray
+from matplotlib.figure import Figure
+from matplotlib.pyplot import savefig, show, subplots
+from dslabs_functions import define_grid, HEIGHT
+from dslabs_functions import plot_bar_chart
 
-filename = "data/class_pos_covid.csv"
-file_tag = "health"
-data: DataFrame = read_csv(filename, na_values="")
+covidData = read_csv("data/class_pos_covid.csv", na_values="")
 
-'''
-#Dimensionality Records vs Variables ---------------------------------------
 
-print(data.shape)
+def plot_records_vs_variables(data: DataFrame, file_tag: str):
+    print("Plotting nr. records vs nr. variables for", file_tag)
+    figure(figsize=(7, 5))
+    values: dict[str, int] = {
+        "nr records": data.shape[0], "nr variables": data.shape[1]}
+    plot_bar_chart(
+        list(values.keys()),
+        list(values.values()),
+        title="Nr of records vs nr variables"
+    )
+    savefig(f"images/{file_tag}_records_variables.png")
 
-figure(figsize=(7, 5))
-values: dict[str, int] = {"nr records": data.shape[0], "nr variables": data.shape[1]}
-plot_bar_chart(
-    list(values.keys()), list(values.values()), title="Nr of records vs nr variables"
-)
-savefig(f"images/{file_tag}_records_variables.png")
-#show()
 
-'''
+def plot_missing_data(data: DataFrame, file_tag: str):
+    print("Plotting missing data for", file_tag)
+    mv: dict[str, int] = {}
+    for var in data.columns:
+        nr: int = data[var].isna().sum()
+        if nr > 0:
+            mv[var] = nr
 
-'''
-# Missing Values ---------------------------------------
-
-mv: dict[str, int] = {}
-for var in data.columns:
-    nr: int = data[var].isna().sum()
-    if nr > 0:
-        mv[var] = nr
-
-figure()
-plot_bar_chart(
-    list(mv.keys()),
-    list(mv.values()),
-    title="Nr of missing values per variable",
-    xlabel="variables",
-    ylabel="nr missing values",
-)
-savefig(f"images/{file_tag}_mv.png")
-show()
-
-'''
-
-'''
-#Data Types-------------------------------------------------------------------
-
-print(data.dtypes)
-
-from pandas import Series, to_numeric, to_datetime
+    figure()
+    plot_bar_chart(
+        list(mv.keys()),
+        list(mv.values()),
+        title="Nr of missing values per variable",
+        xlabel="variables",
+        ylabel="nr missing values",
+    )
+    savefig(f"images/{file_tag}_mv.png")
 
 
 def get_variable_types(df: DataFrame) -> dict[str, list]:
-    variable_types: dict = {"numeric": [], "binary": [], "date": [], "symbolic": []}
+    variable_types: dict = {"numeric": [],
+        "binary": [], "date": [], "symbolic": []}
 
     nr_values: Series = df.nunique(axis=0, dropna=True)
     for c in df.columns:
@@ -72,79 +69,61 @@ def get_variable_types(df: DataFrame) -> dict[str, list]:
 
     return variable_types
 
-variable_types: dict[str, list] = get_variable_types(data)
-print(variable_types)
-counts: dict[str, int] = {}
-for tp in variable_types.keys():
-    counts[tp] = len(variable_types[tp])
 
-figure(figsize=(6, 4))
-plot_bar_chart(
-    list(counts.keys()), list(counts.values()), title="Nr of variables per type"
-)
-savefig(f"images/{file_tag}_variable_types.png")
-#show()
+def plot_variable_types(data: DataFrame, file_tag: str):
+    print("Plotting variable types", file_tag)
+    variable_types: dict[str, list] = get_variable_types(data)
+    print(variable_types)
+    counts: dict[str, int] = {}
+    for tp in variable_types.keys():
+        counts[tp] = len(variable_types[tp])
 
-symbolic: list[str] = variable_types["symbolic"]
-data[symbolic] = data[symbolic].apply(lambda x: x.astype("category"))
-data.dtypes
-'''
-
-'''
-#------------------Data Distribution-------------------------
-from matplotlib.pyplot import savefig, show
-from dslabs_functions import get_variable_types
-from numpy import ndarray
-from matplotlib.figure import Figure
-from matplotlib.pyplot import savefig, show, subplots
-from dslabs_functions import define_grid, HEIGHT
-from dslabs_functions import plot_bar_chart
-
-
-#-------------BoxPlots----------------------
-
-summary5: DataFrame = data.describe(include="all")
-print(summary5)
-
-
-variables_types: dict[str, list] = get_variable_types(data)
-numeric: list[str] = variables_types["numeric"]
-if [] != numeric:
-    data[numeric].boxplot(rot=45)
-    savefig(f"images/{file_tag}_global_boxplot.png")
-    show()
-else:
-    print("There are no numeric variables.")
-
-
-if [] != numeric:
-    rows: int
-    cols: int
-    rows, cols = define_grid(len(numeric))
-    fig: Figure
-    axs: ndarray
-    fig, axs = subplots(
-        rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT), squeeze=False
+    figure(figsize=(6, 4))
+    plot_bar_chart(
+        list(counts.keys()), list(counts.values()), title="Nr of variables per type"
     )
-    i, j = 0, 0
-    for n in range(len(numeric)):
-        axs[i, j].set_title("Boxplot for %s" % numeric[n])
-        axs[i, j].boxplot(data[numeric[n]].dropna().values)
-        i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
-    savefig(f"images/{file_tag}_single_boxplots.png")
-    show()
-else:
-    print("There are no numeric variables.")
+    savefig(f"images/{file_tag}_variable_types.png")
 
-#-------Outliers-----------------------------------------------------It is possible to change the boundaries for the outlier definition
 
-from pandas import Series
-from matplotlib.pyplot import figure, savefig, show
-from dslabs_functions import plot_multibar_chart
+# symbolic: list[str] = variable_types["symbolic"]
+# data[symbolic] = data[symbolic].apply(lambda x: x.astype("category"))
+# data.dtypes
+
+
+def plot_box_plots(data: DataFrame, file_tag: str):
+    print("Plotting box plot for", file_tag)
+    variables_types: dict[str, list] = get_variable_types(data)
+    numeric: list[str] = variables_types["numeric"]
+    if [] != numeric:
+        data[numeric].boxplot(rot=45)
+        savefig(f"images/{file_tag}_global_boxplot.png")
+        show()
+    else:
+        print("There are no numeric variables.")
+
+    if [] != numeric:
+        rows: int
+        cols: int
+        rows, cols = define_grid(len(numeric))
+        axs: ndarray
+        _, axs = subplots(
+            rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT), squeeze=False
+        )
+        i, j = 0, 0
+        for n in range(len(numeric)):
+            axs[i, j].set_title("Boxplot for %s" % numeric[n])
+            axs[i, j].boxplot(data[numeric[n]].dropna().values)
+            i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
+        savefig(f"images/{file_tag}_single_boxplots.png")
+        show()
+    else:
+        print("There are no numeric variables.")
+
 
 NR_STDEV: int = 2
 IQR_FACTOR: float = 1.5
 
+summary5: DataFrame = data.describe(include="all")
 
 def determine_outlier_thresholds_for_var(
     summary5: Series, std_based: bool = True, threshold: float = NR_STDEV
@@ -180,14 +159,16 @@ def count_outliers(
             summary5[var], std_based=True, threshold=nrstdev
         )
         outliers_stdev += [
-            data[data[var] > top].count()[var] + data[data[var] < bottom].count()[var]
+            data[data[var] > top].count()[var] + data[data[var]
+                                        < bottom].count()[var]
         ]
 
         top, bottom = determine_outlier_thresholds_for_var(
             summary5[var], std_based=False, threshold=iqrfactor
         )
         outliers_iqr += [
-            data[data[var] > top].count()[var] + data[data[var] < bottom].count()[var]
+            data[data[var] > top].count()[var] + data[data[var]
+                                        < bottom].count()[var]
         ]
 
     return {"iqr": outliers_iqr, "stdev": outliers_stdev}
@@ -210,9 +191,7 @@ else:
     print("There are no numeric variables.")
 
 
-#-----------------------Histograms---------
-
-from dslabs_functions import set_chart_labels
+# -----------------------Histograms---------
 
 
 if [] != numeric:
@@ -238,7 +217,6 @@ else:
 
 # histogram symbolic
 
-from dslabs_functions import plot_bar_chart
 
 symbolic: list[str] = variables_types["symbolic"] + variables_types["binary"]
 if [] != symbolic:
@@ -319,7 +297,8 @@ if [] != vars:
         var1: str = vars[i]
         for j in range(i + 1, len(vars)):
             var2: str = vars[j]
-            plot_multi_scatters_chart(data, var1, var2, target, ax=axs[i, j - 1])
+            plot_multi_scatters_chart(
+                data, var1, var2, target, ax=axs[i, j - 1])
     savefig(f"images/{file_tag}_sparsity_per_class_study.png")
     show()
 else:
